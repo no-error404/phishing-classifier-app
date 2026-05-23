1813cd7eabee9cd418fa60e7a92da0539e5fa300
no-error404 <nosleep512@protonmail.com>
2026-05-23 23:44:43 +1000

    changed to throw an error instead of return

--- validator.js
+++ validator.js
@@ -2,7 +2,7 @@ function validateEmail(emailInput){
     const requiredFields = ["to", "from", "subject", "message"];
     for (const field of requiredFields) {
         if (!emailInput[field]) {
-            return { valid: false, error: `Missing required field: ${field}` };
+            throw new Error(`Invalid Email: Missing required field: ${field}`);
         }
     }
 
